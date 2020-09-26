187b9212fda049baf40ac380fa0a3690e72cc2cc
aubrey-heim <aubrey.l.heim@gmail.com>
2020-09-26 16:08:51 -0700

    added question array

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,40 @@ var timerEl = document.getElementById("timer")
 var startEl = document.getElementById("start-button")
 var mainEl = document.querySelector("main")
 
+var questionAnswers = [
+    {"question": "What tag is used for a hyperlink?", 
+    "option1": "<link>", 
+    "option2": "<ul>", 
+    "option3": "<p>", 
+    "option4": "<a>",
+    "correct": "<a>" },
+    
+    {"question": "What is Lorem Ipsum?", 
+    "option1": "A coding language", 
+    "option2": "A coding accreditation ", 
+    "option3": "A traditional placeholder text", 
+    "option4": "A well-known coding university",
+    "correct": "A traditional placeholder text" },
+    
+    {"question": "What special character should be used at the end of every statement?", 
+    "option1": "period", 
+    "option2": "semi-colon", 
+    "option3": "bracket", 
+    "option4": "colon",
+    "correct": "semi-colon" },
+    
+    {"question": "What data type stores true or false values? ", 
+    "option1": "boolean", 
+    "option2": "string", 
+    "option3": "integer", 
+    "option4": "char",
+    "correct": "boolean" }
+]
+    
+
+
+
+
 function startTime() {
     var timeLeft = 59;
     var timeInterval = setInterval(function() {
